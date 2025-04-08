019279706aaac641e6a0e3edd289eaf9af7c74ce
EPI28 <35221507+EPI28@users.noreply.github.com>
2025-04-08 18:06:06 +0200

    Update schoenen.py

--- schoenen.py
+++ schoenen.py
@@ -1,7 +1,16 @@
+import streamlit as st
 import pandas as pd
 
 # Lees het CSV-bestand in
 df = pd.read_csv('exclusieve_schoenen_verkoop_met_locatie.csv')
 
+st.set_page_config(page_title="Schoenen Verkoop", layout="wide")
+
+st.title("📊 Exclusieve Schoenen Verkoop met Locatie")
+
+
 # Toon de eerste rijen van de dataset
 print(df.head())
+
+
+
